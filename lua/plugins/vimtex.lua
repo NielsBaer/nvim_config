710af2aac67aef8228b829ebce2b0360ecb29e5c
NielsBaer <niels.behr@gmx.de>
2025-07-03 11:37:58 +0200

    before luasnip

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -4,6 +4,10 @@ return {
   -- tag = "v2.15", -- uncomment to pin to a specific release
   init = function()
     -- VimTeX configuration goes here, e.g.
-    vim.g.vimtex_view_method = 'zathura'
+    vim.g.vimtex_view_method = 'zathura_simple'
+    vim.g.vimtex_quickfix_autoclose_after_keystrokes = 1
+    vim.g.vimtex_compiler_latexmk = {
+      continuous = 0,
+    }
   end,
 }
